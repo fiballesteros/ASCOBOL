29aa3f017d080c920eafe2b0128d13387c6c695a
Faith Ballesteros <patrickstarforever01@gmail.com>
2017-02-25 12:21:58 +0800

    Basic COBOL programs

---
+++ COBOL Programs/shortest.cob
@@ -0,0 +1,13 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.  ShortestProgram.
+
+       PROCEDURE DIVISION.
+       DisplayPrompt.
+       DISPLAY "I did it".
+       STOP RUN.
