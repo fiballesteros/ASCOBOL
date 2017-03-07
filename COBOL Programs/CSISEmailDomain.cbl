c47a134d225dab45bb7a08ef94156794416c13cf
Faith Ballesteros <patrickstarforever01@gmail.com>
2017-03-07 10:17:28 +0800

    Template Done!

---
+++ COBOL Programs/CSISEmailDomain.cbl
@@ -0,0 +1,18 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. CSISEmailDomain.
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            STOP RUN.
+       END PROGRAM CSISEmailDomain.
