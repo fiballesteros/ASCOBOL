4abf5bc61fa20d4e6502a8e5fc7418d7e7fd6316
Faith Ballesteros <patrickstarforever01@gmail.com>
2017-03-07 10:39:34 +0800

    Designation for the work file (Done!)

--- COBOL Programs/CSISEmailDomain.cbl
+++ COBOL Programs/CSISEmailDomain.cbl
@@ -9,6 +9,15 @@
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
+
+       SELECT GraduateInfo ASSIGN TO "Gradinfo.Dat"
+           ORGANIZATION IS LINE SEQUENTIAL.
+       SELECT CountryInfo  ASSIGN TO "CountryCodes.Dat"
+           ORGANIZATION IS LINE SEQUENTIAL.
+       SELECT SortedFile ASSIGN TO "SortedDomain.Dat"
+           ORGANIZATION IS LINE SEQUENTIAL.
+       SELECT WorkFile ASSIGN TO "WORK.TMP".
+
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
