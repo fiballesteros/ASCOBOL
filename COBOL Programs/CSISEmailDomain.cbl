ecb89ef146da392f15db848920a3b7db2804d08b
Faith Ballesteros <patrickstarforever01@gmail.com>
2017-03-10 18:27:54 +0800

    Procedure Division done, need to make methods in my next commits

--- COBOL Programs/CSISEmailDomain.cbl
+++ COBOL Programs/CSISEmailDomain.cbl
@@ -9,7 +9,6 @@
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
-
        SELECT GraduateInfoFile ASSIGN TO "GradInfo.Dat"
            ORGANIZATION IS LINE SEQUENTIAL.
        SELECT CountryFile  ASSIGN TO "CountryCodes.Dat"
@@ -17,40 +16,38 @@
        SELECT SortedDomainFile ASSIGN TO "SortedDomain.Dat"
            ORGANIZATION IS LINE SEQUENTIAL.
        SELECT WorkFile ASSIGN TO "WORK.TMP".
-
        DATA DIVISION.
        FILE SECTION.
         FD CountryFile.
-       01 CountryRec            PIC X(28).
-        88 EndOfCountryFile   VALUE HIGH-VALUES.
-
+           01 CountryRec            PIC X(28).
+           88 EndOfCountryFile   VALUE HIGH-VALUES.
        FD GraduateInfoFile.
-        01 GradInfoRecGF.
-       88 EndOfGradFile      VALUE HIGH-VALUES.
-       02 StudentNameGF      PIC X(25).
-       02 GradYearGF         PIC 9(4).
-        02 CourseCodeGF       PIC 9.
-        88 CSISGraduate    VALUE 1 THRU 5.
-         02 EmailAddrGF        PIC X(28).
-        02 EmailDomainGF      PIC X(20).
-        02 CountryCodeGF      PIC XX.
+       01 GradInfoRecGF.
+           88 EndOfGradFile      VALUE HIGH-VALUES.
+           02 StudentNameGF      PIC X(25).
+           02 GradYearGF         PIC 9(4).
+           02 CourseCodeGF       PIC 9.
+           88 CSISGraduate    VALUE 1 THRU 5.
+           02 EmailAddrGF        PIC X(28).
+           02 EmailDomainGF      PIC X(20).
+           02 CountryCodeGF      PIC XX.
 
        FD SortedDomainFile.
        01 GradInfoRecSF.
-        02 EmailDomainSF      PIC X(20).
+           02 EmailDomainSF      PIC X(20).
            02 StudentNameSF      PIC X(25).
-         02 GradYearSF         PIC 9(4).
-        02 CourseNameSF       PIC X(25).
-        02 CountryNameSF      PIC X(26).
+           02 GradYearSF         PIC 9(4).
+           02 CourseNameSF       PIC X(25).
+           02 CountryNameSF      PIC X(26).
 
        SD WorkFile.
        01 WorkRec.
-        88 EndOfWorkFile      VALUE HIGH-VALUES.
-        02 StudentNameWF      PIC X(25).
-        02 GradYearWF         PIC 9(4).
-        02 CourseCodeWF       PIC 9.
-        02 EmailDomainWF      PIC X(20).
-        02 CountryCodeWF      PIC XX.
+           88 EndOfWorkFile      VALUE HIGH-VALUES.
+           02 StudentNameWF      PIC X(25).
+           02 GradYearWF         PIC 9(4).
+           02 CourseCodeWF       PIC 9.
+           02 EmailDomainWF      PIC X(20).
+           02 CountryCodeWF      PIC XX.
 
        WORKING-STORAGE SECTION.
        01 CourseTable.
@@ -62,13 +59,16 @@
             03 FILLER         PIC X(25) VALUE "Computing with French".
        02 FILLER REDEFINES CourseValue.
             03 CourseName      PIC X(5).
-
        01 CountryTable.
            02 Country.
            03 CountryCode    PIC X(2).
            03 CountryName    PIC X(26).
 
        PROCEDURE DIVISION.
-       MAIN-PROCEDURE.
-            STOP RUN.
+       Begin.
+       SORT WorkFile ON ASCENDING KEY EmailDomainWF
+        INPUT PROCEDURE  IS SelectCSISGraduates
+        OUTPUT PROCEDURE IS MakeEmailDomainFile.
+       STOP RUN.
+
        END PROGRAM CSISEmailDomain.
