3a3900fde5ea5aa618e539777a977ba47988697c
Faith Ballesteros <patrickstarforever01@gmail.com>
2017-03-10 21:34:09 +0800

    File-control section done!

--- COBOL Programs/AromaSalesRPT.cbl
+++ COBOL Programs/AromaSalesRPT.cbl
@@ -9,6 +9,13 @@
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
+           SELECT SalesFile ASSIGN TO "Sales.dat"
+                 ORGANIZATION IS LINE SEQUENTIAL.
+           SELECT SummaryReport ASSIGN TO "AromaSales.rpt"
+                 ORGANIZATION IS LINE SEQUENTIAL.
+           SELECT SortFile ASSIGN TO "SortSale.dat"
+                 ORGANIZATION IS LINE SEQUENTIAL.
+           SELECT WorkFile ASSIGN TO "SORT.TMP".
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
