12621b605418b415eb1b1c8a8bff4d3e286c321d
Faith Ballesteros <patrickstarforever01@gmail.com>
2017-03-10 19:50:53 +0800

    Error - Cannot be subscripted :(

--- COBOL Programs/CSISEmailDomain.cbl
+++ COBOL Programs/CSISEmailDomain.cbl
@@ -16,6 +16,7 @@
        SELECT SortedDomainFile ASSIGN TO "SortedDomain.Dat"
            ORGANIZATION IS LINE SEQUENTIAL.
        SELECT WorkFile ASSIGN TO "WORK.TMP".
+
        DATA DIVISION.
        FILE SECTION.
         FD CountryFile.
@@ -59,15 +60,16 @@
             03 FILLER         PIC X(25) VALUE "Computing with French".
        02 FILLER REDEFINES CourseValue.
             03 CourseName      PIC X(5).
-       01 CountryTable.
-           02 Country.
-           03 CountryCode    PIC X(2).
+
+           01 CountryTable.
+          02 Country OCCURS 243 TIMES INDEXED BY Cidx.
+        03 CountryCode    PIC XX.
            03 CountryName    PIC X(26).
 
        PROCEDURE DIVISION.
        SORT WorkFile ON ASCENDING KEY EmailDomainWF
         INPUT PROCEDURE  IS CSISGraduates
-        OUTPUT PROCEDURE IS MakeEmailDomainFile.
+        OUTPUT PROCEDURE IS Email.
        STOP RUN.
 
        CSISGraduates.
@@ -92,4 +94,41 @@
          END-PERFORM
         CLOSE GraduateInfoFile.
 
-       END PROGRAM CSISEmailDomain.
+       Email.
+       PERFORM CreateCountryTable.
+           OPEN OUTPUT SortedDomainFile
+           RETURN WorkFile
+            AT END SET EndOfWorkFile TO TRUE
+       END-RETURN
+       PERFORM UNTIL EndOfWorkFile
+          MOVE StudentNameWF TO StudentNameSF
+          MOVE GradYearWF    TO GradYearSF
+          MOVE CourseName(CourseCodeWF) TO CourseNameSF
+          MOVE EmailDomainWF TO EmailDomainSF
+          SET Cidx TO 1
+          SEARCH Country
+             AT END MOVE "Code not found" TO CountryNameSF
+             WHEN CountryCode(Cidx) = CountryCodeWF
+                  MOVE CountryName(Cidx) TO CountryNameSF
+          END-SEARCH
+          WRITE GradInfoRecSF
+          RETURN WorkFile
+             AT END SET EndOfWorkFile TO TRUE
+          END-RETURN
+       END-PERFORM
+       CLOSE SortedDomainFile.
+
+       CreateCountryTable.
+       OPEN INPUT CountryFile
+       READ CountryFile
+         AT END SET EndOfCountryFile TO TRUE
+       END-READ
+       PERFORM VARYING Cidx FROM 1 BY 1 UNTIL EndOfCountryFile
+         MOVE CountryRec TO Country(Cidx)
+         READ CountryFile
+           AT END SET EndOfCountryFile TO TRUE
+        END-READ
+       END-PERFORM.
+       CLOSE CountryFile.
+       CLOSE SortedDomainFile.
+       Exit.
