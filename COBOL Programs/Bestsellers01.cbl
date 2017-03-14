0ff94299afa56c2a4371da1c00efe188081fe65f
Faith Ballesteros <patrickstarforever01@gmail.com>
2017-03-14 10:34:13 +0800

    Created a layout for the program

---
+++ COBOL Programs/Bestsellers01.cbl
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. BESTSELLERS01.
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       END PROGRAM BESTSELLERS01.
