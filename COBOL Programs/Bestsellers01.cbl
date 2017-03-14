a1966b99346948899db785e0d7408debd54e8ffe
Faith Ballesteros <patrickstarforever01@gmail.com>
2017-03-14 10:50:58 +0800

    Variables declared!

--- COBOL Programs/Bestsellers01.cbl
+++ COBOL Programs/Bestsellers01.cbl
@@ -9,10 +9,10 @@
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
-           SELECT CountryFile  ASSIGN TO "BOOKSALES.DAT"
+           SELECT BookMasterFile  ASSIGN TO "BOOKSALES.DAT"
                ORGANIZATION IS LINE SEQUENTIAL.
 
-           SELECT GraduateInfoFile ASSIGN TO "BOOKMF.DAT"
+           SELECT BookSalesFile ASSIGN TO "BOOKMF.DAT"
                ORGANIZATION IS LINE SEQUENTIAL.
 
            SELECT WorkFile ASSIGN TO "WORK.TMP".
@@ -22,6 +22,33 @@
 
        DATA DIVISION.
        FILE SECTION.
+       FD BookMasterFile.
+       01  BMF-Record.
+           02 BMF-BookNum        PIC X(5).
+           02 BMF-BookTitle      PIC X(25).
+           02 BMF-AuthorName     PIC X(25).
+
+       FD  BookSalesFile.
+       01  BSF-Record.
+       88 EndOfBSF VALUE HIGH-VALUES.
+               02 BSF-BookNum        PIC X(5).
+               02 BSF-Copies         PIC 99.
+               02 BSF-SaleStatus     PIC X.
+       88    NormalSale VALUE "N".
+
+
+       SD WorkFile.
+           01  WorkRec.
+           88 EndOfWorkFile VALUE HIGH-VALUES.
+               02 W-BookNum          PIC X(5).
+               02 W-Copies           PIC 99.
+               02 FILLER             PIC X.
+
+       FD ReportFile.
+           01  PrintLine             PIC X(75).
+
+
+
        WORKING-STORAGE SECTION.
        PROCEDURE DIVISION.
        END PROGRAM BESTSELLERS01.
