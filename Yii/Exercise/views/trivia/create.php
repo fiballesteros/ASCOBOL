0a50424659f7a2980432206196d008258d128304
Faith Ballesteros <patrickstarforever01@gmail.com>
2017-04-07 11:52:25 +0900

    Add files via upload

--- Yii/Exercise/views/trivia/create.php
+++ Yii/Exercise/views/trivia/create.php
@@ -12,10 +12,10 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="trivia-create">
 
-    <h1><?= Html::encode($this->title)
-        'model' => $model]) ?>
-
-</div>
-    ?></h1>
+    <h1><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
